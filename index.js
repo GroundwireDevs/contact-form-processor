e04c783eac8cfeeb9f6cf5240b8e8b2d82db309d
Kenan Scott <kenan@kenanscott.com>
2017-07-18 15:35:12 -0600

    Modify

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    callback(null, 'Hello from Lambda');
+    
+    let emailData = [];
+    for (const key of Object.keys(event)) {
+        emailData.push(key + ': ' + event[key]); // Adds each key and value from the event to the emailData array
+    }
+
+    emailData = emailData.join("\r\n"); // Converts the array into a string
+    
+    const subject = emailData.replace(/\s+/g, ' ').split(' ').slice(0, 8).join(' ');
+
+    ses.sendEmail({
+        Destination: {ToAddresses: [process.env.TO_ADDRESS]},
+        Message: {
+            Body: {Text: {Data: emailData, Charset: 'UTF-8'}},
+            Subject: {Data: subject, Charset: 'UTF-8'}
+        },
+        Source: process.env.FROM_ADDRESS
+        }, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            context.fail(err);
+            return;
+        }
+
+        console.log(data);
+        callback(null, {'successMsg': process.env.SUCCESS_MESSAGE});
+    });
+    
 };

559d8cb97969e2e151783fb5d0a5d0a702e7fb3a
Kenan Scott <kenan@kenanscott.com>
2017-07-20 09:04:13 -0600

    Get from and to address based on event.formKey and env variables

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const AWS = require('aws-sdk');
-const ses = new AWS.SES();
+const ses = new AWS.SES(); // SES for email delivery
 
 exports.handler = (event, context, callback) => {
     
-    let emailData = [];
+    const fromAddress = process.env['FROM_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
+    const toAddress = process.env['TO_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
+
+    let emailData = []; // Declares emailData variable which will have all the form data added to it.
     for (const key of Object.keys(event)) {
         emailData.push(key + ': ' + event[key]); // Adds each key and value from the event to the emailData array
     }
 
     emailData = emailData.join("\r\n"); // Converts the array into a string
     
-    const subject = emailData.replace(/\s+/g, ' ').split(' ').slice(0, 8).join(' ');
+    const subject = emailData.replace(/\s+/g, ' ').split(' ').slice(0, 8).join(' '); // Sets the subject to be the first eight words of the email data
 
-    ses.sendEmail({
-        Destination: {ToAddresses: [process.env.TO_ADDRESS]},
+    ses.sendEmail({ // Sends email
+        Destination: {ToAddresses: [toAddress]},
         Message: {
             Body: {Text: {Data: emailData, Charset: 'UTF-8'}},
             Subject: {Data: subject, Charset: 'UTF-8'}
         },
-        Source: process.env.FROM_ADDRESS
+        Source: fromAddress
         }, function (err, data) {
         if (err) {
             console.log(err, err.stack);
-            callback(err);
+            callback(err); // Sends error information if SES has a problem
             return;
         }
-
         console.log(data);
-        callback(null, {'successMsg': process.env.SUCCESS_MESSAGE});
+        callback(null, {'successMsg': process.env.SUCCESS_MESSAGE}); // Sends success message
     });
-    
 };

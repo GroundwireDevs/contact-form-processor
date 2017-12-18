311c78d2780d842203423c7e0277b6286622e284
Kenan Scott <kenan@kenanscott.com>
2017-12-18 13:55:18 -0700

    Change indents

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const AWS = require('aws-sdk');
 const ses = new AWS.SES(); // SES for email delivery
 
 exports.handler = (event, context, callback) => {
-    
-    const fromAddress = process.env['FROM_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
-    const toAddress = process.env['TO_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
 
-    let emailData = []; // Declares emailData variable which will have all the form data added to it.
-    for (const key of Object.keys(event)) {
-        emailData.push(key + ': ' + event[key]); // Adds each key and value from the event to the emailData array
-    }
+	const fromAddress = process.env['FROM_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
+	const toAddress = process.env['TO_ADDRESS_' + event.formKey]; // Sets from address, based on the formKey input
 
-    emailData = emailData.join("\r\n"); // Converts the array into a string
+	let emailData = []; // Declares emailData variable which will have all the form data added to it.
+	for (const key of Object.keys(event)) {
+		emailData.push(key + ': ' + event[key]); // Adds each key and value from the event to the emailData array
+	}
 
-    let subject = emailData.replace(/\s+/g, ' ').split(' ').slice(0, 8).join(' '); // Sets the subject to be the first eight words of the email data
-    subject = process.env['APP_' + event.formKey] + ' | ' + subject; // Start subject with the app name, set by env variable
+	emailData = emailData.join('\r\n'); // Converts the array into a string
 
-    ses.sendEmail({ // Sends email
-        Destination: {ToAddresses: [toAddress]},
-        Message: {
-            Body: {Text: {Data: emailData, Charset: 'UTF-8'}},
-            Subject: {Data: subject, Charset: 'UTF-8'}
-        },
-        Source: fromAddress
-        }, function (err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            callback({'errorMessage': process.env.FAILURE_MESSAGE}); // Sends generic error message if SES has a problem
-            return;
-        }
-        console.log(data);
-        callback(null, {'successMsg': process.env.SUCCESS_MESSAGE}); // Sends success message
-    });
+	let subject = emailData.replace(/\s+/g, ' ').split(' ').slice(0, 8).join(' '); // Sets the subject to be the first eight words of the email data
+	subject = process.env['APP_' + event.formKey] + ' | ' + subject; // Start subject with the app name, set by env variable
+
+	ses.sendEmail({ // Sends email
+		Destination: {ToAddresses: [toAddress]},
+		Message: {
+			Body: {Text: {Data: emailData, Charset: 'UTF-8'}},
+			Subject: {Data: subject, Charset: 'UTF-8'}
+		},
+		Source: fromAddress
+	}, function (err, data) {
+		if (err) {
+			console.log(err, err.stack);
+			callback({'errorMessage': process.env.FAILURE_MESSAGE}); // Sends generic error message if SES has a problem
+			return;
+		}
+		console.log(data);
+		callback(null, {'successMsg': process.env.SUCCESS_MESSAGE}); // Sends success message
+	});
 };

fa95bfe0f50db09fdbac319124531b8719fe7f3b
Kenan Scott <kenan@kenanscott.com>
2017-12-19 13:30:56 -0700

    Add X-Ray

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const ses = new AWS.SES();
 const config = require('./config.json');
 
